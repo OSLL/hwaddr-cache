fb8526d8a339e5265eeddaf413c51f6869f46193
Vlad Grozin <glukobug@ya.ru>
2014-03-14 05:14:39 +0400

    fix stub name

--- hwaddr-hashtable.h
+++ hwaddr-hashtable.h
@@ -4,14 +4,14 @@
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,1,0)
 
 #include "hashtable_old.h"
-#define hash_stub_for_each_rcu(name, obj, node, member, key) hash_for_each_possible_rcu(name, obj, node, member, key)
-#define hash_stub_for_each_safe(name, bkt, node, tmp, obj, member) hash_for_each_safe(name, bkt, node, tmp, obj, member)
+#define hwaddr_hash_for_each_rcu(name, obj, node, member, key) hash_for_each_possible_rcu(name, obj, node, member, key)
+#define hwaddr_hash_for_each_safe(name, bkt, node, tmp, obj, member) hash_for_each_safe(name, bkt, node, tmp, obj, member)
 
 #else
 
 #include <linux/hashtable.h>
-#define hash_stub_for_each_rcu(name, obj, node, member, key) hash_for_each_possible_rcu(name, obj, member, key)
-#define hash_stub_for_each_safe(name, bkt, node, tmp, obj, member) hash_for_each_safe(name, bkt, tmp, obj, member)
+#define hwaddr_hash_for_each_rcu(name, obj, node, member, key) hash_for_each_possible_rcu(name, obj, member, key)
+#define hwaddr_hash_for_each_safe(name, bkt, node, tmp, obj, member) hash_for_each_safe(name, bkt, tmp, obj, member)
 
 #endif /*LINUX_VERSION_CODE < KERNEL_VERSION(3,1,0)*/
 
