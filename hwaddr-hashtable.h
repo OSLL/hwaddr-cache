0dc5b3b6036e76f27f9e33dc19869e54d3fe3c2a
mike krinkin <krinkin.m.u@gmail.com>
2014-04-14 13:26:29 +0400

    Revert "hashtable interface fixes"
    This reverts commit f664c28c8e09d75b1c05a811efd78d37a54d061b.

--- hwaddr-hashtable.h
+++ hwaddr-hashtable.h
@@ -4,28 +4,14 @@
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,1,0)
 
 #include "hashtable_old.h"
-
-#define hwaddr_hash_for_each_possible_rcu(name, obj, node, member, key) \
-	hash_for_each_possible_rcu(name, obj, node, member, key)
-
-#define hwaddr_hash_for_each_rcu(name, bkt, node, obj, member) \
-	hash_for_each_rcu(name, bkt, node, obj, member)
-
-#define hwaddr_hash_for_each_safe(name, bkt, node, tmp, obj, member) \
-	hash_for_each_safe(name, bkt, node, tmp, obj, member)
+#define hwaddr_hash_for_each_rcu(name, obj, node, member, key) hash_for_each_possible_rcu(name, obj, node, member, key)
+#define hwaddr_hash_for_each_safe(name, bkt, node, tmp, obj, member) hash_for_each_safe(name, bkt, node, tmp, obj, member)
 
 #else
 
 #include <linux/hashtable.h>
-
-#define hwaddr_hash_for_each_possible_rcu(name, obj, node, member, key) \
-	hash_for_each_possible_rcu(name, obj, member, key)
-
-#define hwaddr_hash_for_each_rcu(name, bkt, node, obj, member) \
-	hash_for_each_rcu(name, bkt, obj, member)
-
-#define hwaddr_hash_for_each_safe(name, bkt, node, tmp, obj, member) \
-	hash_for_each_safe(name, bkt, tmp, obj, member)
+#define hwaddr_hash_for_each_rcu(name, obj, node, member, key) hash_for_each_possible_rcu(name, obj, member, key)
+#define hwaddr_hash_for_each_safe(name, bkt, node, tmp, obj, member) hash_for_each_safe(name, bkt, tmp, obj, member)
 
 #endif /*LINUX_VERSION_CODE < KERNEL_VERSION(3,1,0)*/
 
